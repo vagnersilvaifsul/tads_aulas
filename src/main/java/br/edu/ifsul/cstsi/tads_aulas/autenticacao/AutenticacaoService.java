8cb84b042d15d7dab01adadc12e6cd62a70c9756
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2024-11-07 06:33:36 -0300

    fix: autenticação com JWT

---
+++ src/main/java/br/edu/ifsul/cstsi/tads_aulas/autenticacao/AutenticacaoService.java
@@ -0,0 +1,22 @@
+package br.edu.ifsul.cstsi.tads_aulas.autenticacao;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+//indica que essa classe deve ser adicionada ao Contexto do aplicativo como um Bean da camada de serviço de dados
+public class AutenticacaoService implements UserDetailsService {
+    private final AutenticacaoRepository rep;
+
+    //indica ao Spring Boot que ele deve injetar essa dependência para a classe funcionar
+    public AutenticacaoService(AutenticacaoRepository rep) {
+        this.rep = rep;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return rep.findByEmail(username);
+    }
+}
