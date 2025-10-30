d8152845222fcb5f1ad358ec8a8eb106b8b3605b
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2025-10-30 09:13:09 -0300

    feat: validation

--- src/main/java/br/edu/ifsul/cstsi/tads_aulas/autenticacao/UsuarioAutenticacaoDTO.java
+++ src/main/java/br/edu/ifsul/cstsi/tads_aulas/autenticacao/UsuarioAutenticacaoDTO.java
@@ -1,6 +1,21 @@
 package br.edu.ifsul.cstsi.tads_aulas.autenticacao;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+/*
+  /^
+  (?=.*\d)              // deve conter ao menos um dígito
+  (?=.*[a-z])           // deve conter ao menos uma letra minúscula
+  (?=.*[A-Z])           // deve conter ao menos uma letra maiúscula
+  (?=.*[$*&@#])         // deve conter ao menos um caractere especial
+  [0-9a-zA-Z$*&@#]{8,}  // deve conter ao menos 8 dos caracteres mencionados
+$/
+*/
+
 public record UsuarioAutenticacaoDTO(
+        @Email
         String email,
+        @Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$", message = "A senha deve conter ao menos uma letra maiúscula, uma letra minúscula, um númeral, um caractere especial e um total de 8 caracteres.")
         String senha) {
 }
