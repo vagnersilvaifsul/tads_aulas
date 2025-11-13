7ae4d7513157cd6a2c297e563240cac05ee3c557
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2025-11-13 10:04:42 -0300

    feat: teste, preparação do projeto e do banco de dados

--- src/test/java/br/edu/ifsul/cstsi/tads_aulas/TadsAulasApplicationTests.java
+++ src/test/java/br/edu/ifsul/cstsi/tads_aulas/TadsAulasApplicationTests.java
@@ -2,8 +2,10 @@ package br.edu.ifsul.cstsi.tads_aulas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TadsAulasApplicationTests {
 
 	@Test
