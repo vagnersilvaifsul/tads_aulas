4143de72f9fe6e1d84d3e6587e7a5ff762c164eb
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-04-09 13:26:02 -0300

    semana 6: construção de controllers e métodos na Repository

---
+++ src/main/java/br/edu/ifsul/cstsi/tads_aulas/produto/ProdutoController.java
@@ -0,0 +1,47 @@
+package br.edu.ifsul.cstsi.tads_aulas.produto;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/produtos")
+public class ProdutoController {
+
+    private final ProdutoRepository produtoRepository;
+
+    // Injeção das dependências da classe ProdutoController.
+    // Em outras palavras, quais são as dependências necessárias para a classe ProdutoController funcionar
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll(){
+        return ResponseEntity.ok(produtoRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id){
+        Optional<Produto> produto = produtoRepository.findById(id);
+        if(produto.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(produto.get());
+    }
+
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<List<Produto>> findByName(@PathVariable String nome){
+        Optional<List<Produto>> produtos = produtoRepository.findByNome(nome);
+        if(produtos.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(produtos.get());
+    }
+}
--- src/main/java/br/edu/ifsul/cstsi/tads_aulas/produto/ProdutoRepository.java
+++ src/main/java/br/edu/ifsul/cstsi/tads_aulas/produto/ProdutoRepository.java
@@ -1,6 +1,17 @@
 package br.edu.ifsul.cstsi.tads_aulas.produto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+import java.util.Optional;
+
+
+@RepositoryRestResource(exported = false) //Esta anotação desliga a geração automática de controller pelo Spring Rest
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    //método em Query Speak (em JPQL ou SQL)
+    //método em Domain Speak (só pelo nome do método, o SQL é gerado pelo Spring Data a partir da assinatura do método.)
+    @Query("SELECT p FROM Produto p WHERE p.nome LIKE :nome%")
+    Optional<List<Produto>> findByNome(String nome);
 }
