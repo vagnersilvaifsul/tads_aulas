63706c7f93525781000f1cfbe8e4b0bbea1b1c75
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-05-13 17:24:05 -0300

    semana 8: fix da implementação do JWT.

--- src/main/java/br/edu/ifsul/cstsi/tads_aulas/infra/doc/SpringDocConfiguration.java
+++ src/main/java/br/edu/ifsul/cstsi/tads_aulas/infra/doc/SpringDocConfiguration.java
@@ -1,9 +1,12 @@
 package br.edu.ifsul.cstsi.tads_aulas.infra.doc;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +16,14 @@ public class SpringDocConfiguration {
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement()
+                    .addList(securitySchemeName))
+            .components(new Components()
+                    .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")))
             .info(new Info()
                 .title("API de Exemplo Desenvolvida para as Aulas de TADS")
                 .description("API Rest da aplicação tads_produtos_springboot_exemplo, contendo as funcionalidades de CRUD de produtos.")
--- src/main/java/br/edu/ifsul/cstsi/tads_aulas/infra/security/SecurityConfig.java
+++ src/main/java/br/edu/ifsul/cstsi/tads_aulas/infra/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,12 +43,11 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable()) //desabilita a proteção contra ataques Cross-site Request Forger
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> {
+            authorize.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
             authorize.requestMatchers(HttpMethod.POST, "/api/v1/login").permitAll(); //exceto, a rota de login
             authorize.anyRequest().authenticated(); //demais rotas devem ser autenticadas
             })
-            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-            .httpBasic(Customizer.withDefaults());
-
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
